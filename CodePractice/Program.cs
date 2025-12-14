859ae51913078006f1bb5830dd22bcadd7e6d3f5
Abiel Jesrrel (Relly) Delgado Lee <jesrrel1989.1989@gmail.com>
2025-12-14 12:43:53 -0500

    First commit

---
+++ CodePractice/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using CodePractice;
+
+namespace RetosCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TeoremaPitagoras tp = new TeoremaPitagoras();
+
+            Console.WriteLine("Ingrese el valor del cateto A: ");
+            double catetoA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor del cateto B: ");
+            double catetoB = Convert.ToDouble(Console.ReadLine());
+
+            double area = tp.CalcularArea(catetoA, catetoB);
+
+            Console.WriteLine("El área del triángulo rectángulo es: {0} \n\n\n", area);
+
+
+            Console.WriteLine("Calculando la hipotenusa");
+
+            Console.WriteLine("Ingrese el valor del cateto A: ");
+            catetoA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del cateto B: ");
+            catetoB = Convert.ToDouble(Console.ReadLine());
+
+            double hipotenusa = tp.CalcularHipotenusa(catetoA, catetoB);
+
+            Console.WriteLine("La hipotenusa del triángulo rectángulo es: {0} \n\n\n", hipotenusa);
+
+        }
+    }
+}
---
+++ CodePractice/TeoremaPitagoras.cs
@@ -0,0 +1,52 @@
+﻿namespace CodePractice
+{
+    public class TeoremaPitagoras
+    {
+        public double CalcularHipotenusa(double catetoA, double catetoB)
+        {
+            return Math.Sqrt(catetoA * catetoA + catetoB * catetoB);
+        }
+
+        public double CalcularCateto(double hipotenusa, double cateto)
+        {
+            return Math.Sqrt(hipotenusa * hipotenusa - cateto * cateto);
+        }
+
+        public bool VerificarTrianguloRetangulo(double ladoA, double ladoB, double ladoC)
+        {
+            double[] lados = new double[] { ladoA, ladoB, ladoC };
+            Array.Sort(lados);
+            return Math.Abs((lados[0] * lados[0] + lados[1] * lados[1]) - (lados[2] * lados[2])) < 1e-10;
+        }
+
+        public double CalcularArea(double catetoA, double catetoB)
+        {
+            return (catetoA * catetoB) / 2;
+        }
+
+        public double CalcularPerimetro(double catetoA, double catetoB)
+        {
+            double hipotenusa = CalcularHipotenusa(catetoA, catetoB);
+            return catetoA + catetoB + hipotenusa;
+        }
+
+        public double CalcularAltura(double catetoA, double catetoB)
+        {
+            double hipotenusa = CalcularHipotenusa(catetoA, catetoB);
+            return (catetoA * catetoB) / hipotenusa;
+        }
+
+        public double CalcularAnguloA(double catetoA, double catetoB)
+        {
+            double hipotenusa = CalcularHipotenusa(catetoA, catetoB);
+            return Math.Asin(catetoA / hipotenusa) * (180.0 / Math.PI);
+        }
+
+        public double CalcularAnguloB(double catetoA, double catetoB)
+        {
+            double hipotenusa = CalcularHipotenusa(catetoA, catetoB);
+            return Math.Asin(catetoB / hipotenusa) * (180.0 / Math.PI);
+        }
+
+    }
+}
